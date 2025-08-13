4dbfbd0f2b770d1e91522a355b28d6c3eae5c3e5
fish-minkyu <e951219@naver.com>
2025-08-13 19:56:09 +0900

    Add Main10815

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    int sum = IntStream.of(1, 3, 5, 7, 9)
-        .peek(System.out::println)
-        .sum();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int N = Integer.parseInt(br.readLine());
+      String[] myCardString = br.readLine().split(" ");
+
+      // Set 사용
+      Set<Integer> myCards = new HashSet<>();
+      for (int i = 0; i < N; i++) {
+          myCards.add(Integer.parseInt(myCardString[i]));
+      }
+
+      int M = Integer.parseInt(br.readLine());
+      String[] targetCards = br.readLine().split(" ");
+
+      StringBuilder answer = new StringBuilder();
+
+      for (int i = 0; i < M; i++) {
+        int result = myCards.contains(Integer.parseInt(targetCards[i])) ? 1 : 0;
+        answer.append(result).append(" ");
+      }
+
+      System.out.println(answer);
   }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int solution(int a, int b) {
-        StringBuilder sb1 = new StringBuilder();
-        String str1 = sb1.append(a).append(b).toString();
+    public int solution(int[][] board) {
 
-        StringBuilder sb2 = new StringBuilder();
-        String str2 = sb2.append(b).append(a).toString();
+        int n = board.length;
+        int answer = n * n;
+        List<List<Integer>> dangerous = new ArrayList<>();
 
-        int num1 = Integer.parseInt(str1);
-        int num2 = Integer.parseInt(str2);
+        // {좌상단, 상단, 우상단, 좌측, 우측, 좌하단, 하단, 우하단} 순서
+        int[] dY = {-1, -1, -1,  0,  0,  1,  1,  1}; // row 변화량
+        int[] dX = {-1,  0,  1, -1,  1, -1,  0,  1}; // column 변화량
 
-        return num1 > num2 ? num1 : num2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 지뢰를 발견하면
+                if (board[i][j] == 1) {
+                    // 8방향으로 확인
+                    List<Integer> bomb = new ArrayList<>();
+                    bomb.add(i);
+                    bomb.add(j);
+                    if (!dangerous.contains(bomb)) dangerous.add(bomb);
+
+                    for (int k = 0; k < dY.length; k++) {
+                        int y = i + dY[k];
+                        int x = j + dX[k];
+                        // y, x의 값이 board 안에 있는지 확인
+                        boolean withInHeight = 0 <= y && y < n;
+                        boolean withInWidth = 0 <= x && x < n;
+
+                        // 범위를 벗어나면 pass
+                        if (!(withInHeight && withInWidth)) continue;
+
+                        // 범위 안에 들어갔지만 dangerous에 이미 있는 위험지역이라면 pass
+                        List<Integer> area = new ArrayList<>();
+                        area.add(y);
+                        area.add(x);
+
+                        if (dangerous.contains(area)) continue;
+
+                        // dangerous 2차원 배열에 추가
+                        dangerous.add(area);
+                    }
+                }
+            }
+        }
+
+        return answer - dangerous.toArray().length;
     }
 }
